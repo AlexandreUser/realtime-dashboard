3be3e3d181a92766a9934537c1616eaa0252f1d6
AlexandreUser <av.souza2018@gmail.com>
2021-02-04 14:25:13 -0300

    added persistent zoom

--- src/components/generic-timeseries.js
+++ src/components/generic-timeseries.js
@@ -1,20 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts"
 import ApexCharts from "apexcharts"
 export default function GenericChart(props){
     let dates = props.timeSeries
-
+    const [zoomed,setZoomed] = useState(null)
     useEffect(()=>{
         dates = props.timeSeries
         window.setInterval(() => {
            
-            
-            ApexCharts.exec('realtime', 'updateSeries', [{
-              dates
-            }])
+         
+
+            if(zoomed && zoomed.xaxis){
+              ApexCharts.exec('realtime', 'updateOptions', {
+                series:dates,
+                xaxis: {
+                  ...zoomed
+                }
+              });
+
+            }
           }, 1000)
     },[props.timeSeries])
-    let data =  {
+    let data =  {}
+    if(!zoomed){
+      data = {
           
         series: dates,
         options: {
@@ -25,7 +34,17 @@ export default function GenericChart(props){
                   speed: 1000
                 }
               },
+              noData: {
+                text: "Loading..."
+              },
+              
           chart: {
+            events: {
+              zoomed: function (chartContext, options) {
+                setZoomed(options)
+                console.log(options)
+              }
+            },
             type: 'area',
             stacked: false,
             height: 350,
@@ -80,9 +99,91 @@ export default function GenericChart(props){
             }
           }
         },
+      }
+    }
+  
+    else{
+      data = {
+          
+        series: dates,
+        options: {
+            animations: {
+                enabled: true,
+                easing: 'linear',
+                dynamicAnimation: {
+                  speed: 1000
+                }
+              },
+              noData: {
+                text: "Loading..."
+              },
+              
+          chart: {
+            events: {
+              zoomed: function (chartContext, options) {
+                setZoomed(options)
+                console.log(options)
+              }
+            },
+            type: 'area',
+            stacked: false,
+            height: 350,
+            zoom: {
+              type: 'x',
+              enabled: true,
+              autoScaleYaxis: true
+            },
+            toolbar: {
+              autoSelected: 'zoom'
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          markers: {
+            size: 0,
+          },
+          title: {
+            text: 'Weight and positive value',
+            align: 'left'
+          },
+          fill: {
+            type: 'gradient',
+            gradient: {
+              shadeIntensity: 1,
+              inverseColors: false,
+              opacityFrom: 0.8,
+              opacityTo: 0.4,
+              stops: [0, 90, 100]
+            },
+          },
+          yaxis: {
+            labels: {
+              formatter: function (val) {
+                return (val ).toFixed(2)
+              },
+            },
+            title: {
+              text: 'Value'
+            },
+          },
+          xaxis: {
+            type: 'datetime',
+            ...zoomed.xaxis
+          },
+          tooltip: {
+            shared: false,
+            y: {
+              formatter: function (val) {
+                return (val ).toFixed(2)
+              }
+            }
+          }
+        }
+      }
+    }
       
-      
-      };
+
     
     
       return <div id="chart">
